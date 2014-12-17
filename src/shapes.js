5091747be2d433804cb6f79764661c1968ed15ba
TheConnMan <bcconn2112@gmail.com>
2014-12-16 22:37:21 -0500

    Shapes move.

--- src/shapes.js
+++ src/shapes.js
@@ -1,5 +1,28 @@
+var defaultPhysics = {
+	dx: function(d, r) { return r; },
+	dy: function(d, r) { return r; },
+	x: function(d, w, h) { return d.x; },
+	y: function(d, w, h) { return d.y; },
+	rx: function(d, w, h) { return (Math.PI - d.angle) % (2 * Math.PI); },
+	ry: function(d, w, h) { return 2 * Math.PI - d.angle; },
+	xx: function(d, v, r) { return v / 10 * Math.cos(d.angle); },
+	yy: function(d, v, r) { return v / 10 * Math.sin(d.angle); }
+}, ghostPhysics = {
+	dx: function(d, r) { return 0; },
+	dy: function(d, r) { return 0; },
+	x: function(d, w, h) { return (d.x + w) % w; },
+	y: function(d, w, h) { return (d.y + h) % h; },
+	rx: function(d, w, h) { return d.angle; },
+	ry: function(d, w, h) { return d.angle; },
+	xx: function(d, v, r) { return v * Math.cos(d.angle); },
+	yy: function(d, v, r) { return v * Math.sin(d.angle); }
+};
+
 $(function() {
-	start({count: 20});
+	start({
+		count: 100,
+		momentum: 20
+	});
 })
 
 var defaults = {
@@ -7,7 +30,7 @@ var defaults = {
 	radius: 10,
 	spines: 10,
 	spineSize: 5,
-	velocity: 10
+	velocity: 1
 };
 
 function start(options) {
@@ -26,7 +49,7 @@ function start(options) {
 			color: colors(d),
 			x: Math.random() * (width - 2 * (radius + spineSize)) + radius + spineSize,
 			y: Math.random() * (height - 2 * (radius + spineSize)) + radius + spineSize,
-			v: opts.velocity,
+			velocity: opts.velocity,
 			angle: 2 * Math.PI * Math.random()
 		}
 	});
@@ -47,5 +70,40 @@ function start(options) {
 				.attr('y1', Math.sin(angle) * data.radius)
 				.attr('y2', Math.sin(angle) * (data.radius + data.spineSize));
 		});
-	})
+	});
+	
+	var start;
+	
+	function step(timestamp) {
+		if (!start) {
+			start = timestamp;
+		}
+		var progress = timestamp - start;
+		
+		refresh(progress);
+		
+		start = timestamp;
+		requestAnimationFrame(step)
+	}
+	
+	function refresh(progress) {
+		shapes.forEach(function(d) {
+			var r = d.radius + d.spineSize;
+			var v = d.velocity;
+			var physics = defaultPhysics;
+			if ((d.y >= innerHeight - physics.dy(d, r) && Math.sin(d.angle) > 0) || (d.y <= physics.dy(d, r) && Math.sin(d.angle) < 0)) {
+				d.y = physics.y(d, innerWidth, innerHeight);
+				d.angle = physics.ry(d, innerWidth, innerHeight);
+			}
+			if ((d.x >= innerWidth - physics.dx(d, r) && Math.cos(d.angle) > 0) || (d.x <= physics.dx(d, r) && Math.cos(d.angle) < 0)) {
+				d.x = physics.x(d, innerWidth, innerHeight);
+				d.angle = physics.rx(d, innerWidth, innerHeight);
+			}
+			d.x += physics.xx(d, v, r) * progress;
+			d.y += physics.yy(d, v, r) * progress;
+		});
+		svgShapes.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+	}
+	
+	requestAnimationFrame(step);
 }
